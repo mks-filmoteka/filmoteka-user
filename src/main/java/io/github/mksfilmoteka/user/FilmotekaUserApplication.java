5614c0d2ef6524e88af4f364e37ea00ca7b3d84f
Maks <maxsouldrake@gmail.com>
2026-07-29 14:03:58 +0200

    initial commit

---
+++ src/main/java/io/github/mksfilmoteka/user/FilmotekaUserApplication.java
@@ -0,0 +1,13 @@
+package io.github.mksfilmoteka.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmotekaUserApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(FilmotekaUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/mksfilmoteka/user/FilmotekaUserApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mksfilmoteka.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmotekaUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
